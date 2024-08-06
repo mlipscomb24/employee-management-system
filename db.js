456f8e6b00020f905e2cc9125abd95c0c6d5440f
Miranda Lipscomb <mbennett.sa@outlook.com>
2024-08-05 17:28:01 -0700

    updated db to fix the issue with render

--- db.js
+++ db.js
@@ -1,8 +1,13 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 class DB {
-  constructor(config) {
-    this.client = new Client(config);
+  constructor() {
+    this.client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
   }
 
   async connect() {
@@ -14,7 +19,7 @@ class DB {
   }
 
   async viewAllDepartments() {
-    const query = 'SELECT * FROM department ORDER BY id';
+    const query = "SELECT * FROM department ORDER BY id";
     const result = await this.client.query(query);
     return result.rows;
   }
@@ -51,43 +56,55 @@ class DB {
   }
 
   async addDepartment(name) {
-    const query = 'INSERT INTO department (name) VALUES ($1) RETURNING *';
+    const query = "INSERT INTO department (name) VALUES ($1) RETURNING *";
     const result = await this.client.query(query, [name]);
     return result.rows[0];
   }
 
   async addRole(title, salary, departmentId) {
-    const query = 'INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3) RETURNING *';
-    const result = await this.client.query(query, [title, salary, departmentId]);
+    const query =
+      "INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3) RETURNING *";
+    const result = await this.client.query(query, [
+      title,
+      salary,
+      departmentId,
+    ]);
     return result.rows[0];
   }
 
   async addEmployee(firstName, lastName, roleId, managerId) {
-    const query = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4) RETURNING *';
-    const result = await this.client.query(query, [firstName, lastName, roleId, managerId]);
+    const query =
+      "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4) RETURNING *";
+    const result = await this.client.query(query, [
+      firstName,
+      lastName,
+      roleId,
+      managerId,
+    ]);
     return result.rows[0];
   }
 
   async updateEmployeeRole(employeeId, roleId) {
-    const query = 'UPDATE employee SET role_id = $1 WHERE id = $2 RETURNING *';
+    const query = "UPDATE employee SET role_id = $1 WHERE id = $2 RETURNING *";
     const result = await this.client.query(query, [roleId, employeeId]);
     return result.rows[0];
   }
 
   async getDepartments() {
-    const query = 'SELECT id, name FROM department ORDER BY name';
+    const query = "SELECT id, name FROM department ORDER BY name";
     const result = await this.client.query(query);
     return result.rows;
   }
 
   async getRoles() {
-    const query = 'SELECT id, title FROM role ORDER BY title';
+    const query = "SELECT id, title FROM role ORDER BY title";
     const result = await this.client.query(query);
     return result.rows;
   }
 
   async getEmployees() {
-    const query = 'SELECT id, CONCAT(first_name, \' \', last_name) AS name FROM employee ORDER BY last_name, first_name';
+    const query =
+      "SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM employee ORDER BY last_name, first_name";
     const result = await this.client.query(query);
     return result.rows;
   }
